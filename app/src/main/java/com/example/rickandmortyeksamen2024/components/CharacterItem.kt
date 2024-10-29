172e80b2a50569d7d6e5820e36f40a51c9bbf7cd
Sattar <sattar_saud@hotmail.com>
2024-10-29 11:50:41 +0100

    added hahaha

--- app/src/main/java/com/example/rickandmortyeksamen2024/components/CharacterItem.kt
+++ app/src/main/java/com/example/rickandmortyeksamen2024/components/CharacterItem.kt
@@ -14,6 +14,7 @@ fun CharacterItem(character: Character) {
         Text(text = character.name)
         //AsyncImage
         Text(text = character.species)
+        Text(text = "hahaha")
     }
 }
 
