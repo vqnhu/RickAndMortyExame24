64e703e8d3aa5c608b54a7bbcc033d95c5e434b0
Sattar <sattar_saud@hotmail.com>
2024-10-29 16:09:53 +0100

    added logic

--- app/src/main/java/com/example/rickandmortyeksamen2024/data/CharacterList.kt
+++ app/src/main/java/com/example/rickandmortyeksamen2024/data/CharacterList.kt
@@ -1,7 +1,13 @@
 package com.example.rickandmortyeksamen2024.data
 
-import android.app.appsearch.SearchResults
-
 data class CharacterList(
+    val info: Info,
     val results: List<Character>
+)
+
+data class Info(
+    val count: Int,      // Total number of characters
+    val pages: Int,      // Total number of pages
+    val next: String?,    // URL of the next page (null if there is no next page)
+    val prev: String?     // URL of the previous page (null if there is no previous page)
 )
--- app/src/main/java/com/example/rickandmortyeksamen2024/data/RickAndMortyService.kt
+++ app/src/main/java/com/example/rickandmortyeksamen2024/data/RickAndMortyService.kt
@@ -3,14 +3,20 @@ package com.example.rickandmortyeksamen2024.data
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface RickAndMortyService {
     @GET("character/{id}")
     suspend fun getCharacterById(
         @Path("id") id: Int
-    ) : Response<Character>
-
+    ): Response<Character>
 
     @GET("character")
-    suspend fun getAllCharacters(): Response<CharacterList> // This fetches all characters
+    suspend fun getAllCharacters(
+        @Query("page") page: Int = 1
+    ): Response<CharacterList>
 }
+
+
+
+
