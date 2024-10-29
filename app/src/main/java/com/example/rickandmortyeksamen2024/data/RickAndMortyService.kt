1be6f86636ea16de78a9854f13000baa91a1fefb
nhuquynhvu <nhu_92@hotmail.com>
2024-10-29 12:10:52 +0100

    need to debug

--- app/src/main/java/com/example/rickandmortyeksamen2024/data/RickAndMortyService.kt
+++ app/src/main/java/com/example/rickandmortyeksamen2024/data/RickAndMortyService.kt
@@ -1,5 +1,12 @@
 package com.example.rickandmortyeksamen2024.data
 
-interface RickAndMortyService {
+import okhttp3.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
 
+interface RickAndMortyService {
+    @GET("character/{id}")
+    suspend fun getCharacterById(
+        @Path("id") id: Int
+    ) : Response<Character>
 }
